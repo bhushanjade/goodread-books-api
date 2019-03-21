fc4620672a9ca3845d14260f0f1d00f849d593b7
bjade <bjade@gracenote.com>
2019-03-21 11:26:05 +0530

    load env and morgan on dev enviornment only

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const request = require('request');
 
-const morgan = require('morgan');
-
-//read .env
-// require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+    const morgan = require('morgan');
+    require('dotenv').config();
+}
 
 const cors = require('cors');
 const PORT = process.env.PORT || '5000';
@@ -17,8 +17,10 @@ const BOOK_SEARCH_PATH = 'search/index.xml';
 const BOOK_SHOW_PATH = 'book/show/';
 
 app.use(cors());
-
-app.use(morgan('combined'))
+//todo : Find A workaround.
+if (process.env.NODE_ENV !== 'production') {
+    app.use(morgan('combined'))
+}
 /*
 * Basic Server which call goodread API.
 * Only SUpported path 
@@ -29,59 +31,59 @@ app.use(morgan('combined'))
 */
 app.get('/search/books', (req, res) => {
 
-console.log(req.query);
-  let api_path = req.query.path;
+    console.log(req.query);
+    let api_path = req.query.path;
 
-  //Added to limit API misuse. Remove this you can acces any goodread GET API with this.
-  if (api_path.indexOf(BOOK_SEARCH_PATH) > -1) {
-    sendRequest(req, res);
+    //Added to limit API misuse. Remove this you can acces any goodread GET API with this.
+    if (api_path.indexOf(BOOK_SEARCH_PATH) > -1) {
+        sendRequest(req, res);
 
-  } else {
-    sendErrorRequest(res, BOOK_SEARCH_PATH);
-  }
+    } else {
+        sendErrorRequest(res, BOOK_SEARCH_PATH);
+    }
 
 });
 
 
 app.get('/bookDetails', (req, res) => {
 
-  let api_path = req.query.path;
+    let api_path = req.query.path;
 //Added to limit API misuse. Remove this you can acces any goodread GET API with this.
-  if (api_path.indexOf(BOOK_SHOW_PATH) > -1) {
-    sendRequest(req, res);
-  } else {
-    sendErrorRequest(res, BOOK_SHOW_PATH);
-  }
+    if (api_path.indexOf(BOOK_SHOW_PATH) > -1) {
+        sendRequest(req, res);
+    } else {
+        sendErrorRequest(res, BOOK_SHOW_PATH);
+    }
 });
 
 
 /**
- * sendErrorRequest 
- * Only SUpported path 
+ * sendErrorRequest
+ * Only SUpported path
  * BOOK_SEARCH_PATH = 'search/index.xml';
  * BOOK_SHOW_PATH = 'book/show/';
  */
 function sendErrorRequest(res, ALLOWED_PATH) {
-  res.status(400).json({
-    'status': 'Bad Request',
-    "ALLOWED_PATH": ALLOWED_PATH
-  });
+    res.status(400).json({
+        'status': 'Bad Request',
+        "ALLOWED_PATH": ALLOWED_PATH
+    });
 }
 
 function sendRequest(req, res) {
-  let query = req.query;
-  let api_path = query.path;
-  query['key'] = API_KEY;
-  const URL = `${API_URL}/${api_path}`;
-  console.log(req.query);
-  req.pipe(request.get(URL, {
-    'qs': {
-      ...query
-    }
-  })).pipe(res);
+    let query = req.query;
+    let api_path = query.path;
+    query['key'] = API_KEY;
+    const URL = `${API_URL}/${api_path}`;
+    console.log(req.query);
+    req.pipe(request.get(URL, {
+        'qs': {
+            ...query
+        }
+    })).pipe(res);
 }
 
 
 app.listen(PORT, () => {
-  console.log(`server running on port ${PORT}`)
+    console.log(`server running on port ${PORT}`)
 });
